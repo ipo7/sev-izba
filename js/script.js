6206a19e2910ce22dc22cc5f765255e9c551af1a
ipo7 <vektor-inn@yandex.ru>
2020-08-09 13:28:10 +0300

    !

--- js/script.js
+++ js/script.js
@@ -1,4 +1,28 @@
-$(selector).click(function (e) {
-	e.preventDefault();
+
+$(function () {
+
+
+	$(window).resize(function () {
+
+		function rr(e) {
+			// e.preventDefault();
+			let offset = $('.logo-main__image').offset();
+			let top = offset.top;
+			let left = offset.left;
+			console.log(left);
+			if ($(window).width() > 1786) {
+				$('.main__menu, .social-nets').offset({ left: left });
+			} return;
+		};
+		rr();
+	});
+
+	$(window).resize();
+
+
+
+
 
 });
+
+
